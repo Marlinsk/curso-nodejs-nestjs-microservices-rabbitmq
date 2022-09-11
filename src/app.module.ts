a9b96922004674b6212ded187ea34cc89fcfc177
Marlon <Marlonnlmx@outlook.com>
2022-09-11 00:20:33 -0300

    course module 2 code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@machinecloud.fdso3.mongodb.net/smartranking?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+      },
+    ),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
