c3f76a04a82ad47f6d072a635286c8ed8e7ff581
Marlon <Marlonnlmx@outlook.com>
2022-09-11 13:34:24 -0300

    Course module 3 code

--- src/jogadores/schemas/jogador.schema.ts
+++ src/jogadores/schemas/jogador.schema.ts
@@ -2,9 +2,9 @@ import * as mongoose from 'mongoose';
 
 export const JogadorSchema = new mongoose.Schema(
   {
-    telefoneCelular: { type: String, unique: true },
     email: { type: String, unique: true },
     nome: String,
+    telefoneCelular: { type: String },
     ranking: String,
     posicaoRanking: Number,
     urlFotoJogador: String,
